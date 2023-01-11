4cf677cdd44776b998c4fac0d20906f307ca70f2
Olivier Perron <38085309+Makix77@users.noreply.github.com>
2023-01-11 10:06:31 -0500

    Labo0 IFT232

---
+++ Labo0/src/fusee/Dessin.java
@@ -0,0 +1,25 @@
+package fusee;
+
+public class Dessin
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("  /\\");
+        System.out.println(" /__\\");
+        System.out.println(" |  |");
+        System.out.println("/____\\");
+        System.out.println(" /WW\\");
+    }
+    
+    public static void dessinerCargo(String[] args) {
+        
+    }
+    public static void dessinerCiterne(String[] args) {
+            
+    }
+    public static void dessinerMoteur(String[] args) {
+        
+    }
+}
---
+++ Labo0/src/module-info.java
@@ -0,0 +1,3 @@
+module Labo0
+{
+}
